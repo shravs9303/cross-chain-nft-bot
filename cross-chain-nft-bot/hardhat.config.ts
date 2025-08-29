f2100ab33466cf246068499c2369ffcabf63d02f
shravs9303 <115494341+shravs9303@users.noreply.github.com>
2025-08-29 18:23:41 +0530

    Update hardhat.config.ts

---
+++ cross-chain-nft-bot/hardhat.config.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
